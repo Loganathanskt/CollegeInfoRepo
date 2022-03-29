3f2a22568c1eef64e48ea6e6befa4a43421bf70f
Loganathanskt <loganathanskt@gmail.com>
2022-03-29 17:56:27 +0530

    College Project

---
+++ src/org/college/College.java
@@ -0,0 +1,34 @@
+package org.college;
+
+public class College {
+	private void collegeName() {
+		System.out.println("College Name is : SMIT");
+	}
+	private void collegeCode() {
+		System.out.println("College Code is : 123");
+	}
+	private void collegeRank() {
+		System.out.println("College Rank is : 5");
+	}
+public static void main(String[] args) {
+	
+	College c = new College();
+	Student s = new Student();
+	Hostel h = new Hostel();
+	Dept d = new Dept();
+	
+	c.collegeName();
+	c.collegeCode();
+	c.collegeRank();
+	
+	s.studentId();
+	s.studentName();
+	s.studentDept();
+	
+	h.hashCode();
+	h.hostelName();
+	
+	d.deptName();
+	
+}
+}
---
+++ src/org/college/Student.java
@@ -0,0 +1,17 @@
+package org.college;
+
+public class Student {
+//	studentName(),studentDept(),studentId()
+	public void studentName() {
+		System.out.println("Student Name is: Ram");
+	}
+	public void studentDept() {
+		System.out.println("Student Dept is: ECE");
+	}
+
+	public void studentId() {
+		System.out.println("Student Id is: 123");
+	}
+
+
+}
